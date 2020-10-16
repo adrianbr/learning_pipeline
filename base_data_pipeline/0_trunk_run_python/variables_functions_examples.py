44449ed61086cfef4acd7318ae06aca0a06cf233
Adrian <Adrian>
2020-10-16 14:50:11 +0200

    1st

---
+++ base_data_pipeline/0_trunk_run_python/variables_functions_examples.py
@@ -0,0 +1,29 @@
+
+print("let's learn about assigning values to variables, retrieving those values and comparing them")
+input("Press Enter to continue...")
+
+print('setting a to 1')
+a = 1
+input("Press Enter to continue...")
+
+print(a)
+
+input("Press Enter to continue...")
+
+#is a equal to 1?
+print("""a == 1 \n is a equal to 1?""")
+
+input("Press Enter to continue...")
+print(a == 1)
+
+input("Let's define a function...")
+
+def concatenate_strings(string1, string2):
+    return string1 + string2
+
+stringa = input("Enter string 1 ")
+stringb = input("Enter string 2")
+
+input("Your result...")
+
+print(concatenate_strings(stringa, stringb) )
